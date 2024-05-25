58092294e4978b01eb8162ded62865cdc0521450
scottchen98 <scott.chen1117@gmail.com>
2024-05-25 14:18:40 -0700

    add test for incrementing functionality in Counter component

--- src/components/counter.test.tsx
+++ src/components/counter.test.tsx
@@ -1,12 +1,13 @@
-import { render } from "@testing-library/react";
-import { test } from "vitest";
+import { screen, render } from "@testing-library/react";
+import { expect, test } from "vitest";
 import Counter from "./counter";
 
 test("Should render the component", () => {
   render(<Counter />);
 });
 
-test.todo(
-  "Should increment when the 'Increment' button is pressed",
-  async () => {},
-);
+test("Should increment when the 'Increment' button is pressed", async () => {
+  render(<Counter />);
+  const currentCount = screen.getByTestId("current-count");
+  expect(currentCount.textContent).toBe("00");
+});
