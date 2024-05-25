6aca940bb65c237a840dea6861577c42e774b12d
scottchen98 <scott.chen1117@gmail.com>
2024-05-25 16:40:39 -0700

    add tests for initial count and reset button

--- src/components/counter.test.tsx
+++ src/components/counter.test.tsx
@@ -14,3 +14,24 @@ test("Should increment when the 'Increment' button is pressed", async () => {
   await user.click(button);
   expect(currentCount).toHaveTextContent("1");
 });
+
+test("Should reset the count when the 'Reset' button is pressed", async () => {
+  const { user } = render(<Counter />);
+  const currentCount = screen.getByTestId("current-count");
+  expect(currentCount).toHaveTextContent("0");
+
+  const incButton = screen.getByRole("button", { name: "Increment" });
+  await user.click(incButton);
+  expect(currentCount).toHaveTextContent("1");
+
+  const resetButton = screen.getByRole("button", { name: "Reset" });
+  await user.click(resetButton);
+  expect(currentCount).toHaveTextContent("0");
+});
+
+test("Should render the component with a custom initial count", () => {
+  const count = 3;
+  render(<Counter initialCount={count} />);
+  const currentCount = screen.getByTestId("current-count");
+  expect(currentCount).toHaveTextContent(`${count}`);
+});
