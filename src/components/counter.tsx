de5428c9ca1d1633c6ad9319925167f51dfdd3a3
Michael Lei <michaelleie0ca@gmail.com>
2024-05-25 16:26:25 -0700

    Update counter with initial count and reset button

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -3,15 +3,23 @@ import { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+export default function Counter({
+  initialCount = 0,
+}: {
+  initialCount?: number;
+}) {
+  const [count, setCount] = useState(initialCount);
+
   return (
-    <Card className="flex max-w-xs flex-col  items-center justify-center">
+    <Card className="flex flex-col  items-center justify-center border-none shadow-none">
       <CardHeader>
         <CardTitle>{count}</CardTitle>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-2">
         <Button onClick={() => setCount(count + 1)}>Increment</Button>
+        <Button variant="secondary" onClick={() => setCount(initialCount)}>
+          Reset
+        </Button>
       </CardContent>
     </Card>
   );
