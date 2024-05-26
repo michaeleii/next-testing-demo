7326fed81736172de5e22ec92e737763331dc1eb
Michael Lei <michaelleie0ca@gmail.com>
2024-05-25 17:38:54 -0700

    Add back render fn for testing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+import { render as renderComponent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+export function render(
+  jsx: React.ReactElement,
+  options?: Parameters<typeof renderComponent>[1],
+) {
+  return {
+    user: userEvent.setup(),
+    ...renderComponent(jsx, options),
+  };
 }
